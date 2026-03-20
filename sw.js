5d4c35f4205da9085474dcd0ab277ffed2e2e2d3
boomsweet <brid704@gmail.com>
2026-03-20 20:04:08 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const cacheName = "hoptea-v1";
+const filesToCache = [
+  "/customer.html",
+  "/admin.html",
+  "/manifest.json",
+  "/manifest-admin.json",
+  "/sw.js",
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js",
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
+];
+
+self.addEventListener("install", e=>{e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(filesToCache)));});
+self.addEventListener("activate", e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener("fetch", e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
