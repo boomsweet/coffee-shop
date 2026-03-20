df4ea1955b2ddb8c427ea2df851649b147e5e589
boomsweet <brid704@gmail.com>
2026-03-20 20:31:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,4 @@
-const cacheName = "hoptea-v1";
-const filesToCache = [
-  "/customer.html",
-  "/admin.html",
-  "/manifest.json",
-  "/manifest-admin.json",
-  "/sw.js",
-  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js",
-  "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
-];
-
-self.addEventListener("install", e=>{e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(filesToCache)));});
-self.addEventListener("activate", e=>{e.waitUntil(self.clients.claim());});
-self.addEventListener("fetch", e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const CACHE_NAME = 'hop-chafe-v2';
+const assets = ['./', './index.html', './home.html', './pos.html', './admin.html', './manifest.json'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))); });
