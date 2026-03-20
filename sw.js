dfdff4cdbf73311bc54b7960bd624e7f9f6e2f09
boomsweet <brid704@gmail.com>
2026-03-20 18:15:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,2 @@
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open('app').then(c=>c.addAll(['./','./index.html'])));
-});
-self.addEventListener('fetch',e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+self.addEventListener('install', e => { e.waitUntil(caches.open('hoptea-v1').then(cache=>cache.addAll(['/index.html','/customer.html','/admin.html']))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
