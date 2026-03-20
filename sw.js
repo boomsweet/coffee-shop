2d538f931b5562598228151a7da3040e46291fa3
boomsweet <brid704@gmail.com>
2026-03-20 19:31:49 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,26 +3,18 @@ const filesToCache = [
   "/customer.html",
   "/admin.html",
   "/manifest.json",
+  "/manifest-admin.json",
   "/sw.js",
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js",
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
 ];
 
-// Install
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
-  );
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(filesToCache)));
 });
 
-// Activate
-self.addEventListener("activate", e => {
-  e.waitUntil(self.clients.claim());
-});
+self.addEventListener("activate", e=>{e.waitUntil(self.clients.claim());});
 
-// Fetch
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener("fetch", e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
