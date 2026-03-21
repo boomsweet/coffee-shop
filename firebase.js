83e1f49aefe4b21646631852b2f91c547b71dd93
boomsweet <brid704@gmail.com>
2026-03-21 18:33:07 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, collection, addDoc, onSnapshot, doc, updateDoc } 
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { 
+  getFirestore, collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// 🔥 FIX: config ครบ
 const firebaseConfig = {
   apiKey: "AIzaSyCW-ztNX8EAio66VN8ODKo6_z9R8-B99mU",
   authDomain: "coffee-shop-2042e.firebaseapp.com",
-  projectId: "coffee-shop-2042e"
+  databaseURL: "https://coffee-shop-2042e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "coffee-shop-2042e",
+  storageBucket: "coffee-shop-2042e.appspot.com",
+  messagingSenderId: "835937086040",
+  appId: "1:835937086040:web:e7002f925d0b751cadb82d"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, collection, addDoc, onSnapshot, doc, updateDoc };
+// 🔥 export ให้ครบ (สำคัญ)
+export { db, collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc };
