bc25fe90ec5024add51ddf5526d479ea47438143
boomsweet <brid704@gmail.com>
2026-03-21 15:03:23 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, addDoc, onSnapshot } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCW-ztNX8EAio66VN8ODKo6_z9R8-B99mU",
   authDomain: "coffee-shop-2042e.firebaseapp.com",
-  databaseURL: "https://coffee-shop-2042e-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "coffee-shop-2042e",
   storageBucket: "coffee-shop-2042e.firebasestorage.app",
   messagingSenderId: "835937086040",
   appId: "1:835937086040:web:e7002f925d0b751cadb82d"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, addDoc, onSnapshot };
